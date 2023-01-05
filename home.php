debed49d4282fcefe8f2e40161245bd0e3439bdf
allenlin316 <allenlin316@gmail.com>
2023-01-06 00:30:28 +0800

    update search and user_info function

--- home.php
+++ home.php
@@ -35,29 +35,73 @@ if(isset($_SESSION["id"]) && isset($_SESSION["user_name"])){
                 </div>
             </div>
         </div> 
+        <!-- Modal for search -->
+        <div class="modal fade" id="search_icon" tabindex="-1" aria-labelledby="search_icon" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">                    
+                    <h5 class="modal-title" id="search_icon">電子書查詢</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="form-inline my-2 my-lg-0" method="get" action="./home.php">
+                        <input class="form-control mr-sm-2" name="target_book" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>                        
+                    </form>
+                </div>
+                <div class="modal-footer">                    
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>                    
+                </div>
+                </div>
+            </div>
+        </div>
         <div class="jumbotron jumbotron-fluid">
             <button class="btn" data-toggle="modal" data-target="#account_icon"><img src="./images/account_icon.png" alt="account icon" class="rounded-circle bg-white position-absolute" style="width: 4%; left: 34px; top: 25px;"></button>
-            <img src="./images/search_icon.png" alt="search icon" class="position-absolute" style="width: 6%; right: 100px; top: 21px;">  
+            <button class="btn" data-toggle="modal" data-target="#search_icon"><img src="./images/search_icon.png" alt="search icon" class="position-absolute" style="width: 6%; right: 100px; top: 21px;"></button>  
             <img src="./images/heart_icon.png" alt="heart icon" class="position-absolute" style="width: 3%; right: 30px; top: 33px;">          
             <div class="container">
-                <h1 class="display-1 text-center">EBOOK</h1>            
+                <h1 class="display-1 text-center"><a href="./home.php" style="text-decoration: none; color: white">EBOOK</a></h1>            
             </div>
         </div>       
-        <div class="container-fluid">
+        <div class="container-fluid">        
             <div class="row justify-content-around">
                     <?php 
-                    $sql = "SELECT * FROM books";
-                    $result = mysqli_query($conn, $sql);
-                    $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                    $num_of_rows = mysqli_num_rows($result);                    
-                    for($i=0; $i<$num_of_rows; $i++){
+                    if(isset($_GET["target_book"])) {
+                        $target_book = $_GET["target_book"];
+                        $sql = "SELECT * FROM books WHERE `title` = " . "'" . $target_book . "'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                        $num_of_rows = mysqli_num_rows($result);  
+                        if($num_of_rows == 0){
+                            header("Location: home.php?error=找不到此書籍");
+                        }
                         echo "<div class=\"col-md-2 mb-3 card\" style=\"width: 180px; height: auto;\">
-                        <img src=\"./images/{$row[$i]["title"]}.jpg\" style=\"width: 60%; height: 180px;\" class=\"card-img-top mx-auto\" alt=\"atomic_habit book\">
+                        <img src=\"./images/{$row[0]["title"]}.jpg\" style=\"width: 60%; height: 180px;\" class=\"card-img-top mx-auto\" alt=\"atomic_habit book\">
                         <div class=\"card-body\">
-                            <h5 class=\"card-title text-center\">{$row[$i]["title"]}</h5>                            
+                            <h5 class=\"card-title text-center\">{$row[0]["title"]}</h5>                            
                         </div>
                         </div>";
-                    }?>                                    
+                    }
+                    else if(isset($_GET["error"])){
+                        echo "<p class=\"display-4 text-danger\">查無結果請重新搜尋</p>";
+                    }                                         
+                    else{
+                        $sql = "SELECT * FROM books";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                        $num_of_rows = mysqli_num_rows($result);                    
+                        for($i=0; $i<$num_of_rows; $i++){
+                            echo "<div class=\"col-md-2 mb-3 card\" style=\"width: 180px; height: auto;\">
+                            <img src=\"./images/{$row[$i]["title"]}.jpg\" style=\"width: 60%; height: 180px;\" class=\"card-img-top mx-auto\" alt=\"atomic_habit book\">
+                            <div class=\"card-body\">
+                                <h5 class=\"card-title text-center\">{$row[$i]["title"]}</h5>                            
+                            </div>
+                            </div>";
+                        }
+                    } 
+                    ?>                                    
             </div>                
         </div>                
         <script src="./jquery-3.5.1.min.js"></script>

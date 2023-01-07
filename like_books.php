d1ca7c48e6b62c5a8335010473de74c5b294af29
tianyo1110 <tianyo1110@gmail.com>
2023-01-07 16:00:48 +0800

    修改珍藏書移除後的頁面渲染

--- like_books.php
+++ like_books.php
@@ -7,10 +7,12 @@ $book_id = $_POST["book_id"];
 if(isset($_POST["unlike_book"])){ // 取消收藏
     echo "remove book from collections";
     $sql = "DELETE FROM `user_book` WHERE user_id='" . $user_id . "' AND book_id='" . $book_id . "'";
+    header("Location: collections.php");
 }
 else{   // 加入收藏
     echo "add book to collections";   
     $sql = "INSERT INTO `user_book`(`id`, `user_id`, `book_id`) VALUES (NULL, '" . $user_id . "','" . $book_id . "')";
+    header("Location: home.php");
 }
 
 if (mysqli_query($conn, $sql)) {
@@ -20,6 +22,6 @@ else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-header("Location: home.php");
+
 
 ?>
